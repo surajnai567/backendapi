3c3f610c43ba3482534889df9fb832eb66a6692f
surajnai567 <surajnai567@gmail.com>
2021-06-11 01:10:45 +0530

    event models.py

--- event/models.py
+++ event/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from user.models import User
 from django.contrib.postgres.fields import ArrayField
+import os
 
 # Create your models here.
 
 
 class Event(models.Model):
     host_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.URLField()
+    #image = models.URLField()
+    image = models.ImageField(upload_to=os.environ.get("S3_FOLDER", "images"))
     title = models.CharField(max_length=300)
     description = models.CharField(max_length=300)
     location = models.CharField(max_length=300)
