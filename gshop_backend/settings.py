b5f0d4bd5c752fb4499f6d3a8715b50485bceb70
surajnai567 <surajnai567@gmail.com>
2021-06-11 14:43:06 +0530

    settings.py

--- gshop_backend/settings.py
+++ gshop_backend/settings.py
@@ -162,5 +162,6 @@ APPEND_SLASH = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
